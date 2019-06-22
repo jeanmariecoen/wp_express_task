b0c8dd98d6de3a86792ff695d1d935b40346b9c3
Jean-Marie Coen <jeanmariecoen@gmail.com>
2019-06-21 23:41:11 -0400

    nouvelles tentatives pour le ajax du delete

--- taskEspresso/public/javascripts/ajax_create_task.js
+++ taskEspresso/public/javascripts/ajax_create_task.js
@@ -1,42 +1,34 @@
 var liste_taches = document.getElementById("liste_taches");
 document.getElementById("create_task").addEventListener("click", create_task, false);
-console.log('avant');
+
 function create_task(e)
 {
   e.preventDefault();
-  console.log('après');
+
+  // récupérer les values du formulaire
   var titre_tache = document.getElementById("titre_tache").value;
   var description_tache = document.getElementById("description_tache").value;
   // var utilisateur= document.getElementById("utilisateur").value;
 
+  // préparer le json
   var nouvelle_tache = JSON.stringify({'post_title': titre_tache, 'post_content': description_tache});
-  // console.log(nouvelle_tache);
 
+  // préparer la requête
   var xhr = new XMLHttpRequest();
   xhr.open('POST', 'http://localhost/wp-task-jpl/wp-json/taskManager/v0/task', true);
   xhr.setRequestHeader("Content-Type", "application/json");
 
+  // Ajax
   xhr.onreadystatechange = function () 
   {
       if (xhr.readyState == 4) 
       {
           if (xhr.status == 200)
           {
-            // var myObj = JSON.parse(this.responseText);
-            // console.log(xhr.responseText);
-            // console.log('la tâche est créée');
-            // for (var i = 0; i<myObj.length; i++)
-            // {
-           
-            // }
-            // appel à la fonction 1 pour réainitialiser les tâches  
+            mettreAjour();
           };
       };            
   };
 
-  // xhr.open('GET', 'http://localhost/wp-task-jpl/wp-json/taskManager/v0/task', true);
   xhr.send(nouvelle_tache);
-  // liste_taches.innerHTML = "";
-  setTimeout(mettreAjour, 3000)
- 
-}
+};
--- taskEspresso/public/javascripts/ajax_delete.js
+++ taskEspresso/public/javascripts/ajax_delete.js
@@ -1,26 +1,32 @@
+// var liste_taches = document.getElementById("liste_taches");
+boutons_delete = document.getElementsByClassName("supprimer");
 
-function deleteTask(event)
+for (var i = 0; i < boutons_delete.length; ++i)
 {
-    console.log('sfsf');
-    var tacheCliquer=e.currentTarget;
-    var xhr = new XMLHttpRequest();
+    boutons_delete[i].addEventListener("click", delete_task, false)
+}
 
+function delete_task(e)
+{
+    var tacheCliquee = e.currentTarget;
+    var task_id = tacheCliquee.dataset.taskId;
+    console.log(task_id);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('DELETE', 'http://localhost/wp-task-jpl/wp-json/taskManager/v0/task/'+task_id, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    // Ajax
     xhr.onreadystatechange = function () 
     {
         if (xhr.readyState == 4) 
         {
             if (xhr.status == 200)
-            
-            var myObj = tacheCliquer;
-            console.log(tacheCliquer);
-            
-            // boucle pour afficher les titres de toutes les tâches
-           
-        }            
-    }
+            {
+                mettreAjour();
+            };
+        }; 
+    };           
 
-
-    //Envoie le titre du film ou de la série dans la page likes.php
-    xhr.open('DELETE', 'http://localhost/wp-task-jpl/wp-json/taskManager/v0/task', true);
-    xhr.send();        
-    }
+    xhr.send(task_id);   
+};
